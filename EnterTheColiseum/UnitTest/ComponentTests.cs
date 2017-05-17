f0100c7c547c37f6af8496cacd92e373a34c238a
Marc Nielsen <marc2036@student.eadania.dk>
2017-05-17 12:33:07 +0200

    UnitTest added ComponentTests

---
+++ EnterTheColiseum/UnitTest/ComponentTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EnterTheColiseum;
+using Microsoft.Xna.Framework;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ComponentTests
+    {
+        [TestMethod]
+        public void AddComponentTest()
+        {
+            //Arrangement
+            GameObject obj = new GameObject(Vector2.Zero);
+            Collider component = new Collider(obj);
+
+            //Act
+            obj.AddComponent(component);
+
+            //Assert
+            Assert.AreSame(obj.GetComponent("Collider"), component);
+        }
+    }
+}
