14122c5cfa20b00c7d8590caca22a35d8d065528
Marc Nielsen <marc2036@student.eadania.dk>
2017-05-18 10:43:34 +0200

    ...

--- EnterTheColiseum/EnterTheColiseum/Component Pattern/Reaver.cs
+++ EnterTheColiseum/EnterTheColiseum/Component Pattern/Reaver.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace EnterTheColiseum.Component_Pattern
+namespace EnterTheColiseum
 {
-    class Reaver
+    class Reaver : Gladiator
     {
+        //Fields
+        float skillLevel;
+        Thread thread;
+
+        //Properties
+        public float SkillLevel
+        {
+            get { return skillLevel; }
+        }
+
+        //Constructor
+        public Reaver(GameObject gameObject, string name) : base(gameObject, name)
+        {
+        }
+
+        //Methods
+
     }
 }
