d2520a47681ceabe79fd367866ef7d3a959ed758
Marc Nielsen <marc2036@student.eadania.dk>
2017-05-26 12:59:46 +0200

    Use Case 1+2

---
+++ EnterTheColiseum/EnterTheColiseum/Component Pattern/Structures/Barracks.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
+namespace EnterTheColiseum
+{
+    class Barracks : Component, ILoadable, ISaveable
+    {
+        //Fields
+
+        //Component Fields
+        Button button;
+
+        //Properties
+
+        //Constructor
+        public Barracks(GameObject gameObject, Button button) : base(gameObject)
+        {
+            this.button = button;
+        }
+
+        //Method
+        public void LoadContent(ContentManager content)
+        {
+            button.BarracksClicked += Clicked;
+            //Load from database
+        }
+        private void Clicked()
+        {
+            GameObject returnButton = new GameObject(Vector2.Zero);
+            returnButton.AddComponent(new SpriteRenderer(returnButton, "Exitknap", 0.05f, 1));
+            returnButton.AddComponent(new Collider(returnButton, false, false));
+            returnButton.AddComponent(new Button(returnButton, StructureType.Return));
+            (returnButton.GetComponent("SpriteRenderer") as SpriteRenderer).LoadContent(GameWorld.Instance.Content);
+            (returnButton.GetComponent("Collider") as Collider).LoadContent(GameWorld.Instance.Content);
+            GameObject menu = new GameObject(Vector2.Zero);
+            menu.AddComponent(new Menu(menu, (Button)returnButton.GetComponent("Button")));
+
+            GameWorld.Instance.AddGameObject(menu);
+            GameWorld.Instance.AddGameObject(returnButton);
+        }
+        public void Save()
+        {
+            //Save to database
+        }
+    }
+}
---
+++ EnterTheColiseum/EnterTheColiseum/Component Pattern/Structures/Upgrade.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterTheColiseum
+{
+    class Upgrade : Component, ILoadable, ISaveable
+    {
+        //Fields
+
+        //Component Fields
+        Button button;
+
+        //Properties
+
+        //Constructor
+        public Upgrade(GameObject gameObject, Button button) : base(gameObject)
+            {
+            this.button = button;
+        }
+
+        //Method
+        public void LoadContent(ContentManager content)
+        {
+            button.UpgradeClicked += Clicked;
+            //Load from database
+        }
+        private void Clicked()
+        {
+            GameObject returnButton = new GameObject(Vector2.Zero);
+            returnButton.AddComponent(new SpriteRenderer(returnButton, "Exitknap", 0.05f, 1));
+            returnButton.AddComponent(new Collider(returnButton, false, false));
+            returnButton.AddComponent(new Button(returnButton, StructureType.Return));
+            (returnButton.GetComponent("SpriteRenderer") as SpriteRenderer).LoadContent(GameWorld.Instance.Content);
+            (returnButton.GetComponent("Collider") as Collider).LoadContent(GameWorld.Instance.Content);
+            GameObject menu = new GameObject(Vector2.Zero);
+            menu.AddComponent(new Menu(menu, (Button)returnButton.GetComponent("Button")));
+
+            GameWorld.Instance.AddGameObject(menu);
+            GameWorld.Instance.AddGameObject(returnButton);
+        }
+        public void Save()
+        {
+            //Save to database
+        }
+    }
+}
