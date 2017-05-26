dce1e2fda3c59264d5e88ffca7c3f69bacd1c587
Frederik <fehjorth@gmail.com>
2017-05-26 10:00:54 +0200

    Basic Gear architecture

--- EnterTheColiseum/EnterTheColiseum/Component Pattern/Gear.cs
+++ EnterTheColiseum/EnterTheColiseum/Component Pattern/Gear.cs
@@ -8,5 +8,29 @@ namespace EnterTheColiseum
 {
     public class Gear : Component
     {
+        //Fields
+        string name;
+        int armor;
+        int attack;
+
+        //Properties
+        public string Name
+        {
+            get { return name; }
+        }
+        public int Armor
+        {
+            get { return armor; }
+        }
+        public int Attack
+        {
+            get { return attack; }
+        }
+        //Constructor
+
+
+        //Methods
+
+
     }
 }
