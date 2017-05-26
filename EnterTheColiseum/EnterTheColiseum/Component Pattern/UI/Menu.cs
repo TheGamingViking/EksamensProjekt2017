6b19d2e941b702429a4fbc98d3edc1a352c65b44
Marc Nielsen <marc2036@student.eadania.dk>
2017-05-26 11:11:23 +0200

    Use Case - Return followup

---
+++ EnterTheColiseum/EnterTheColiseum/Component Pattern/UI/Menu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace EnterTheColiseum
+{
+    class Menu : UI, ILoadable, IDrawable
+    {
+        //Fields
+        string spritePath = "Menu baggrund";
+        float scale = 0.9f;
+        Texture2D sprite;
+        Rectangle rectangle;
+
+        //Component Fields
+        Button returnButton;
+
+        //Properties
+
+        //Constructor
+        public Menu(GameObject gameObject, Button returnButton) : base(gameObject)
+        {
+            GameObject.Transform.Position = new Vector2(61, 20);
+            this.returnButton = returnButton;
+            LoadContent(GameWorld.Instance.Content);
+        }
+
+        //Methods
+        public void LoadContent(ContentManager content)
+        {
+            sprite = content.Load<Texture2D>(spritePath);
+            rectangle = new Rectangle(0, 0, sprite.Width, sprite.Height);
+            returnButton.ReturnClicked += Clicked;
+        }
+        public void Draw(SpriteBatch spriteBatch, Vector2 position)
+        {
+            spriteBatch.Draw(sprite, position, rectangle, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0.1f);
+        }
+        private void Clicked()
+        {
+            GameWorld.Instance.RemoveGameObject(GameObject);
+            GameWorld.Instance.RemoveGameObject(returnButton.GameObject);
+        }
+    }
+}
