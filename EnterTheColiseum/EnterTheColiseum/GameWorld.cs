822a239a79c8283be9ae6808b1b0ffd22a4c8204
Jeppe Jost Etzerodt <jepp1980@student.eadania.dk>
2017-05-30 14:05:36 +0200

    Removed SQLite

--- EnterTheColiseum/EnterTheColiseum/GameWorld.cs
+++ EnterTheColiseum/EnterTheColiseum/GameWorld.cs
@@ -4,7 +4,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
-using System.Data.SQLite;
+//using System.Data.SQLite;
 
 namespace EnterTheColiseum
 {
